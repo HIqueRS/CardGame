007e4aee1103639c4c9e8b867ef2cd2216f0413e
Hiquers <riquesouzalima11@gmail.com>
2024-10-07 23:00:26 -0300

    Projeto vazio

---
+++ CardGame/Assets/TesteVSC.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesteVSC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.log("Blep");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
